cd83a394353772dc76e5873066cb14c1130cc444
BohdanSavastieievIt <bohdan.savastieiev.it@gmail.com>
2022-07-14 15:57:16 +0300

    lesson 24

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('How many movies you have watched in your life?', '');
+console.log(numberOfFilms);
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,8 +11,32 @@ const personalMovieDB = {
     private: false
 };
 
-const lastMovie = prompt('Один из последних просмотренных фильмов?', ''),
-      lastMovieScore = prompt('На сколько оцениваете его?', '');
-personalMovieDB.movies[lastMovie] = lastMovieScore;
+const watchedMoviesAmount = prompt('How many movies you want to put in the base?');
+
+for(let i = 0; i < watchedMoviesAmount; i++){
+    const lastMovie = prompt('Enter the name of one of the movies you have watched recently?', '');
+    const lastMovieScore = prompt('What is your score for it?', '');
+
+    if (lastMovie != '' && lastMovieScore != '' && lastMovie.length < 50 && lastMovie != null && lastMovieScore != null){
+        personalMovieDB.movies[lastMovie] = lastMovieScore;
+    }
+    else{
+        alert("An error has occured");
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10){
+    alert('Quite a few movies have been watched');
+}
+else if(personalMovieDB.count < 31){
+    alert('You have watched an average amount of movies');
+}
+else if(personalMovieDB.count > 30){
+    alert('You are the cinephile');
+}
+else {
+    alert('An error has occured');
+}
 
 console.log(personalMovieDB.movies);
